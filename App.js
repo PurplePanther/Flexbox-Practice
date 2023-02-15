1834d578d702b655579e0bbbaab64104cea3574f
PurplePanther <45367470+PurplePanther@users.noreply.github.com>
2023-02-15 13:09:42 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <div className="box1">1</div>
+        <div className="box2">2</div>
+        
+          <div className="box3">3</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
